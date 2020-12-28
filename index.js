8a74668617ea0e03c10d582c6cf755c7afb2df63
Kenny I <kenny.o.imarah@gmail.com>
2020-12-28 06:12:48 -0500

    Node Package Manager

---
+++ index.js
@@ -0,0 +1,12 @@
+//jshint esversion:6
+var superheroes = require('superheroes');
+
+//random superheros
+var mySuperheroName = superheroes.random();
+
+// all superheors
+var allSuperheros = superheroes.all;
+
+//console.log(allSuperheros);
+
+console.log(mySuperheroName);
